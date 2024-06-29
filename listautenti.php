407e4c987a6c64b72aede5b8cd154540b2c9a1c8
raghveerdeol <deolraghveer5@gmail.com>
2024-06-29 20:50:59 +0200

    creato array utenti e importata la pagina listautenti in login

---
+++ listautenti.php
@@ -0,0 +1,52 @@
+<?php 
+    $utenti = [
+        [
+            "username" => "luca",
+            "password" => "luca00",
+        ],        
+        [
+            "username" => "mario",
+            "password" => "00Mario00",
+        ],        
+        [
+            "username" => "luigi",
+            "password" => "luigi@99",
+        ],        
+        [
+            "username" => "sasuke",
+            "password" => "vendetta12",
+        ],        
+        [
+            "username" => "naruto",
+            "password" => "naruto1770",
+        ],        
+        [
+            "username" => "inosuke",
+            "password" => "inosuke444",
+        ],        
+        [
+            "username" => "lucifer",
+            "password" => "esekiele666",
+        ],        
+        [
+            "username" => "php",
+            "password" => "fintohtml",
+        ],        
+        [
+            "username" => "cesare",
+            "password" => "brutobrutto",
+        ],        
+        [
+            "username" => "cesare-messicano",
+            "password" => "burrito",
+        ],        
+        [
+            "username" => "gandhi",
+            "password" => "guancia",
+        ],        
+        [
+            "username" => "ravenna",
+            "password" => "cashmeeree",
+        ],
+    ]
+?>
---
+++ login.php
@@ -0,0 +1,39 @@
+<?php 
+// Consegna
+// Dato un array di utenti, ovvero array associativi con username e password in chiaro
+// Creiamo una sicurissima e semplicissima pagina di login in cui l'utente puo' autenticarsi via form con:
+// - un username
+// - una password
+// La pagina di login (login.php) ci redireziona, una volta effettuato e SOLO se avvenuto correttamente,
+// alla pagina principale della nostra applicazione (index.php), in cui visualizziamo via sessione il nome dell'utente loggato.
+// Creiamo poi una pagina di logout (logout.php) che rimuova le informazioni della sessione dalla sessione attuale e redirezioni
+// all pagina di login (login.php).
+// Bonus 1:
+// Impedire la visualizzazione dei contenuti di index.php (anche semplici di prova) fino a quanto l'utente non sia loggato.
+// Bonus 2:
+// Prevedere l'uso di qualsiasi controllo attraverso una funzione inserita in un file separato in 'src/functions.php/') (modificato)
+require_once __DIR__ . "/listautenti.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <form action="./index.php" method="post">
+            <div>
+                <label for="username">Inserire username:</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                    <label for="password">Inserire password:</label>
+                    <input type="text" name="password" id="password">
+            </div>
+            <input type="submit" value="submit">
+        </form>
+    </main>
+</body>
+</html>
